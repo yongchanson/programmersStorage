e97351af1840d6d4a104d2a3bc9fca3a0a5116e5
yongchanson <syc9430@gmail.com>
2022-03-29 18:57:15 +0900

    2016년 풀이추가

--- level-1/2016년.js
+++ level-1/2016년.js
@@ -2,13 +2,29 @@
 //완벽한 정답이 아닙니다.
 //정답 1 - codeisneverodd
 function solution(a, b) {
-    var answer = '';
-    let count = 0
-    const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-    const month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    for (let i = 1; i < a; i++) count += month[i]
-    count += b
-    answer = day[(count + 4) % 7]
+  var answer = "";
+  let count = 0;
+  const day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  for (let i = 1; i < a; i++) count += month[i];
+  count += b;
+  answer = day[(count + 4) % 7];
 
-    return answer;
+  return answer;
+}
+
+//정답 2 - yongchanson
+function solution(a, b) {
+  const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+
+  let sum = b;
+  for (
+    let i = 0;
+    i < a - 1;
+    i++ //ex)5월인 경우 1~4월까지 더해준다.
+  )
+    sum += month[i];
+
+  return week[sum % 7]; //1일이 금요일이므로, 0이면 목요일이 출력되어야 한다.
 }
