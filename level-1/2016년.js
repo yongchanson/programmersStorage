1374953c1663c014646f7618a50c33dd7d90fb19
yongchanson <syc9430@gmail.com>
2022-03-30 02:35:19 +0900

    나머지가 1이되는 수 찾기 풀이

--- level-1/2016년.js
+++ level-1/2016년.js
@@ -19,11 +19,8 @@ function solution(a, b) {
   const week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
 
   let sum = b;
-  for (
-    let i = 0;
-    i < a - 1;
-    i++ //ex)5월인 경우 1~4월까지 더해준다.
-  )
+  for (let i = 0; i < a - 1; i++)
+    //ex)5월인 경우 1~4월까지 더해준다.
     sum += month[i];
 
   return week[sum % 7]; //1일이 금요일이므로, 0이면 목요일이 출력되어야 한다.
--- level-1/나머지가-1이-되는-수-찾기.js
+++ level-1/나머지가-1이-되는-수-찾기.js
@@ -2,9 +2,26 @@
 //완벽한 정답이 아닙니다.
 //정답 1 - codeisneverodd
 function solution(n) {
-    var answer = 0;
-    for (let divisor = n - 1; divisor >= 2; divisor--) {
-        n % divisor === 1 ? answer = divisor : null
-    }
-    return answer;
+  var answer = 0;
+  for (let divisor = n - 1; divisor >= 2; divisor--) {
+    n % divisor === 1 ? (answer = divisor) : null;
+  }
+  return answer;
+}
+
+//정답 2 - yongchanson
+function solution(price, money, count) {
+  var answer = 0;
+  let sum = price;
+
+  for (i = 2; i <= count; i++) {
+    sum += price * i;
+  }
+
+  if (sum <= money) {
+    answer = 0;
+  } else {
+    answer = sum - money;
+  }
+  return answer;
 }
