5233c85a722b2dbfff52c4bdd2056dc90c0b6925
yongchanson <syc9430@gmail.com>
2022-03-30 02:38:19 +0900

    나머지가1이되는수 삭제, 부족한금액찾기 풀이추가

--- level-1/나머지가-1이-되는-수-찾기.js
+++ level-1/나머지가-1이-되는-수-찾기.js
@@ -8,20 +8,3 @@ function solution(n) {
   }
   return answer;
 }
-
-//정답 2 - yongchanson
-function solution(price, money, count) {
-  var answer = 0;
-  let sum = price;
-
-  for (i = 2; i <= count; i++) {
-    sum += price * i;
-  }
-
-  if (sum <= money) {
-    answer = 0;
-  } else {
-    answer = sum - money;
-  }
-  return answer;
-}
--- level-1/부족한-금액-계산하기.js
+++ level-1/부족한-금액-계산하기.js
@@ -2,9 +2,26 @@
 //완벽한 정답이 아닙니다.
 //정답 1 - codeisneverodd
 function solution(price, money, count) {
-    var answer = -1;
-    let totalCost = 0
-    for (let i = 1; i <= count; i++) totalCost += price * i
-    answer = totalCost <= money ? 0 : totalCost - money
-    return answer;
+  var answer = -1;
+  let totalCost = 0;
+  for (let i = 1; i <= count; i++) totalCost += price * i;
+  answer = totalCost <= money ? 0 : totalCost - money;
+  return answer;
+}
+
+//정답 2 - yongchanson
+function solution(price, money, count) {
+  var answer = 0;
+  let sum = price;
+
+  for (i = 2; i <= count; i++) {
+    sum += price * i;
+  }
+
+  if (sum <= money) {
+    answer = 0;
+  } else {
+    answer = sum - money;
+  }
+  return answer;
 }
